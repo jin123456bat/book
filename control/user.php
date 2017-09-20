9f19a994beb59c06b5509013fc63467135452cec
jinchengchen <326550324@qq.com>
2017-09-20 18:28:36 +0800

    提交

--- control/user.php
+++ control/user.php
@@ -3,6 +3,9 @@ namespace book\control;
 use framework\core\control;
 use framework\core\request;
 use framework\core\view;
+use framework\vendor\csrf;
+use framework\core\response\message;
+use framework\core\http;
 
 class user extends control
 {
@@ -10,11 +13,61 @@ class user extends control
 	{
 		if (request::method() == 'post')
 		{
-			
+			$user = new \book\entity\user(request::post());
+			if($user->login())
+			{
+				$user->saveSession();
+				return new message('登录成功',http::url('index', 'index'));
+			}
+			else
+			{
+				return new message('用户名或密码错误');
+			}
 		}
 		else
 		{
 			return new view('user/login.html');
 		}
+	}
+	
+	function register()
+	{
+		if (request::method() == 'post')
+		{
+			$user = new \book\entity\user(request::post());
+			if ($user->validate())
+			{
+				$user->save();
+				return new message('注册成功',http::url('index', 'index'));
+			}
+			else
+			{
+				return new message($user->getError());
+			}
+		}
+		else
+		{
+			return new view('user/register.html');
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\control::__access()
+	 */
+	function __access()
+	{
+		return array(
+			//一个开启csrf验证的例子
+			array(
+				csrf::verify(request::post('token'))?'allow':'deny',
+				'actions' => array(
+					'login',
+					'register',
+				),
+				'message' => new message('请刷新重试'),
+				'express' => request::method() == 'post',
+			),
+		);
 	}
 }
--- entity/user.php
+++ entity/user.php
@@ -2,9 +2,19 @@
 namespace book\entity;
 use framework\core\entity;
 use framework\vendor\encryption;
+use framework\core\session;
 
 class user extends entity
 {
+	static function getUserBySession()
+	{
+		$user = session::get('user');
+		if (!empty($user)) {
+			return new self(array_shift($user));
+		}
+		return null;
+	}
+	
 	private function encrypt($data,$password)
 	{
 		return openssl_encrypt($data, 'aes-256-cbc-hmac-sha256', $password);
@@ -25,6 +35,49 @@ class user extends entity
 		{
 			$this->_data['salt'] = encryption::random(32);
 			$this->_data['password'] = $this->encrypt($this->_data['password'], $this->_data['salt']);
+		}
+	}
+	
+	function login()
+	{
+		if (isset($this->_data['name']) && isset($this->_data['password']))
+		{
+			$old_password = $this->_data['password'];
+			$this->_data = $this->findByFiled('name', $this->_data['name']);
+			if (!empty($this->_data))
+			{
+				if($this->_data['password'] == $this->encrypt($old_password, $this->_data['salt']))
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	function saveSession()
+	{
+		$user = session::get('user');
+		if (empty($user))
+		{
+			$user = array();
+		}
+		$has = false;
+		foreach ($user as $u)
+		{
+			if ($u['id'] == $this->_data['id'])
+			{
+				$has = true;
+				break;
+			}
+		}
+		if (!$has)
+		{
+			$user[] = array(
+				'id' => $this->_data['id'],
+				'name' => $this->_data['name'],
+			);
+			session::set('user', $user);
 		}
 	}
 }
