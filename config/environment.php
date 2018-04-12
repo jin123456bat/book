c2e5fd487ff1db80bbecf97b05e509d836693e76
jin123456bat <326550324@qq.com>
2018-04-12 16:05:07 +0800

    save

--- config/environment.php
+++ config/environment.php
@@ -1,43 +1,8 @@
 <?php
 return array(
-	'display_errors' => 'On',
-	'error_reporting' => E_ALL,
-	'memory_limit' => '128M',
-	
-	'max_execution_time' => 60,
 	'session' => array(
-		'auto_start' => 0,
-		'cookie_lifetime' => 0,
-		'gc_maxlifetime' => 3600,
 		'use_cookies' => 0, // session的传递通过cookie实现
-		'name' => 'framework',
-		
-		'use_trans_sid' => 1, // 禁止url中的session_id
 		'use_only_cookies' => 0, // 只使用cookie中的session_id
-		'cookie_path' => '/',
-		'cookie_domain' => '',
-		'cookie_httponly' => 'On', // 禁止js读取cookie
-		'serialize_handler' => 'php',
-		'gc_probability' => 1,
-		'gc_divisor' => 1000,
-		'referer_check' => '',
-		//'cookie_secure' => 'On',//https
-		'hash_function' => 'sha256'
-		// 只需要配置这2个东西就可以实现session存储在memcached中
-		// 'save_handler' => 'memcached',
-		// 'save_path' => 'localhost:11211',
-		
-		// 只需要配置这2个东西就可以实现session存储在redis中
-		// session.save_handler => 'redis',
-		// session.save_path => "tcp://host1:6379?weight=1, tcp://host2:6379?weight=2&timeout=2.5, tcp://host3:6379?weight=2&read_timeout=2.5"
-	),
-	//设置时区
-	'date' => array(
-		'timezone' => 'Asia/Shanghai'
-	),
-	//开启Gzip
-	'zlib' => array(
-		'output_compression' => 'On',
-		'output_compression_level' => -1,
+		'use_trans_sid' => 1, // 禁止url中的session_id
 	),
 );
